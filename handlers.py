cd2491e48ac48fc66ffb1ecf698e2603767f972a
aisav <aisavikin@gmail.com>
2022-08-24 17:07:24 +0300

    Рефакторинг обработчиков. Повторяющийся код вынесен в отдельные функции.

--- handlers.py
+++ handlers.py
@@ -1,7 +1,6 @@
 from bot import dp
 from aiogram.dispatcher.filters import Text
 from func import *
-from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
@@ -13,6 +12,19 @@ class ForPDAStates(StatesGroup):
 handler_dict = create_data_dict()
 kbrd_default = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True).add('Подписаться', 'Отписаться',
                                                                                 'Статус подписки')
+
+def setup_data(msg):
+    subscribe_status = DataBase().read_one(msg.from_user.id)[1:]
+    now_subscribes = {CATEGORIES[i]: subscribe_status[i] for i in range(len(handler_dict))}
+    data = {'now_subscribes': now_subscribes}
+    return data
+
+def setup_keyboard(data):
+    kbrd = types.InlineKeyboardMarkup()
+    for i in data['now_subscribes']:
+        if not data['now_subscribes'][i]:
+            kbrd.add(handler_dict[i]['btn'])
+    return kbrd
 
 
 @dp.message_handler(commands='start', state='*')
@@ -27,17 +39,12 @@ async def start(msg: types.Message):
 
 
 @dp.message_handler(Text(equals='Подписаться'), state='*')
-async def subscribe(msg: types.Message, state: FSMContext):
+async def subscribe(msg: types.Message):
+    check_user(msg.from_user.id)
     await ForPDAStates.subscribe.set()
-    subscribe_status = DataBase().read_one(msg.from_user.id)[1:]
-    now_subscribes = {CATEGORIES[i]: subscribe_status[i] for i in range(14)}
-    data = {'now_subscribes': now_subscribes}
-    await state.update_data(data=data)
-    kbrd = types.InlineKeyboardMarkup()
-    for i in now_subscribes:
-        if not now_subscribes[i]:
-            kbrd.add(handler_dict[i]['btn'])
-    if all(map(lambda x: now_subscribes[x], now_subscribes)):
+    data = setup_data(msg)
+    kbrd = setup_keyboard(data)
+    if all(map(lambda x: data['now_subscribes'][x], data['now_subscribes'])):
         await msg.answer('Ты подписан на всё что можно', reply_markup=kbrd.add(
             types.InlineKeyboardButton('Отписаться', callback_data='unsubscribe')))
     else:
@@ -45,22 +52,17 @@ async def subscribe(msg: types.Message, state: FSMContext):
 
 
 @dp.callback_query_handler(state=ForPDAStates.subscribe)
-async def subscribe_call(call: types.CallbackQuery, state: FSMContext):
-    await state.update_data(category=call.data)
-    data = await state.get_data()
+async def subscribe_call(call: types.CallbackQuery):
+    data = setup_data(call)
     url = handler_dict[call.data]['url']
-    kbrd = types.InlineKeyboardMarkup()
     item = get_item(url)
-    DataBase().update(data['category'], item, call.from_user.id)
-    now_subscribes = {CATEGORIES[i]: DataBase().read_one(call.from_user.id)[1:][i] for i in range(14)}
-    data = {'now_subscribes': now_subscribes}
-    await state.update_data(data=data)
-    for i in data['now_subscribes']:
-        if not data['now_subscribes'][i]:
-            kbrd.add(handler_dict[i]['btn'])
+    DataBase().update(call.data, item, call.from_user.id)
+    data = setup_data(call)
+    kbrd = setup_keyboard(data)
     if all(map(lambda x: data['now_subscribes'][x], data['now_subscribes'])):
         await call.message.answer('Ты подписан на всё что можно', reply_markup=kbrd.add(
             types.InlineKeyboardButton('Отписаться', callback_data='unsubscribe')))
     else:
         await call.message.answer('Выберите категорию', reply_markup=kbrd)
-    print(data)
+
+
